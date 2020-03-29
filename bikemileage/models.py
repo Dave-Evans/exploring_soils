c5abe0ca37dfd81f035126dbc69bdcaf54cc6286
Dave-Evans <evans.dave.michael@gmail.com>
2020-03-29 14:30:07 -0500

    completed link between mileage and bicycle tables

--- bikemileage/models.py
+++ bikemileage/models.py
@@ -3,35 +3,6 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 
 
-class Mileage(models.Model):
-    # TODO Bike should be separate model class
-    BLUE_TREK = 1
-    RED_TREK = 2
-    TAKARA = 3
-    FUJI = 4
-    OTHER = 6
-    BIKE_TYPES = (
-        (BLUE_TREK, 'Blue Trek'),
-        (RED_TREK, 'Red Trek'),
-        (TAKARA, 'Takara'),
-        (FUJI, 'Fuji'),
-        (OTHER, 'Other'),
-    )
-
-    ride_date   = models.DateField(null=False, blank=False)
-    rider       = models.CharField(max_length=30, blank=True, null=True)
-    mileage     = models.FloatField(null=True)
-    bike_type   = models.PositiveSmallIntegerField(choices=BIKE_TYPES)
-    comment     = models.TextField(blank=True, null=True)
-    cost        = models.FloatField(null=True)
-
-    def get_absolute_url(self):
-        return reverse('custom_mileage')
-        # TODO figure out how to redirect to a sorted page
-        #, kwargs={'pk': self.pk})
-        #sort=-ride_date
-
-
 class Bicycle(models.Model):
 
     name = models.CharField(max_length=30, blank=False, null=False)
@@ -53,3 +24,32 @@ class Bicycle(models.Model):
     def __str__(self):
         return self.name
 
+
+class Mileage(models.Model):
+    # TODO Bike should be separate model class
+    BLUE_TREK = 1
+    RED_TREK = 2
+    TAKARA = 3
+    FUJI = 4
+    OTHER = 6
+    BIKE_TYPES = (
+        (BLUE_TREK, 'Blue Trek'),
+        (RED_TREK, 'Red Trek'),
+        (TAKARA, 'Takara'),
+        (FUJI, 'Fuji'),
+        (OTHER, 'Other'),
+    )
+
+    ride_date   = models.DateField(null=False, blank=False)
+    rider       = models.CharField(max_length=30, blank=True, null=True)
+    mileage     = models.FloatField(null=True)
+    # bike_type   = models.PositiveSmallIntegerField(choices=BIKE_TYPES)
+    bike_type   = models.ForeignKey(Bicycle, models.PROTECT, related_name='mileage')
+    comment     = models.TextField(blank=True, null=True)
+    cost        = models.FloatField(null=True)
+
+    def get_absolute_url(self):
+        return reverse('custom_mileage')
+        # TODO figure out how to redirect to a sorted page
+        #, kwargs={'pk': self.pk})
+        #sort=-ride_date
--- bikemileage/tables.py
+++ bikemileage/tables.py
@@ -5,14 +5,11 @@ from .models import Mileage
 
 
 class Bootstrap4Table(tables.Table):
-    # country = tables.Column(linkify=True)
-    # continent = tables.Column(accessor="country__continent", linkify=True)
 
     class Meta:
         model = Mileage
         template_name = "django_tables2/bootstrap.html"
         attrs = {"class": "table table-hover"}
-        # exclude = ("friendly",)
 
     edit = TemplateColumn(template_name='bikemileage/mileage_update_column.html')
     delete = TemplateColumn(template_name='bikemileage/mileage_delete_column.html')
