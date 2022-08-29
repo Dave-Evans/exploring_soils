af20d2d77cd8b48a0578718fc7f46e62b586e687
Dave-Evans <evans.dave.michael@gmail.com>
2022-08-29 10:53:27 -0500

    delete county file after loading

--- infra/ansible/load_county_data.sh
+++ infra/ansible/load_county_data.sh
@@ -10,3 +10,5 @@ aws s3 cp s3://$4/db/county_nrcs_a_mn.gdb/ ./data/county_nrcs_a_mn.gdb --recursi
 # Load county data to database
 # ogr2ogr -overwrite -f PostgreSQL PG:"host=localhost dbname=db_davemike user=usr_davemike password=luggage12543" -t_srs EPSG:4326 -nln mn_counties ./data/county_nrcs_a_mn.gdb county_nrcs_a_mn
 ogr2ogr -overwrite -f PostgreSQL PG:"host=localhost dbname=$1 user=$2 password=$3" -t_srs EPSG:4326 -nln mn_counties ./data/county_nrcs_a_mn.gdb county_nrcs_a_mn
+
+rm -rf ./data/county_nrcs_a_mn.gdb
