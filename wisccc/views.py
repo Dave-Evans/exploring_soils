3b5b710d175a48c631cfb8e329665e8436d4f349
Dave-Evans <evans.dave.michael@gmail.com>
2023-09-14 14:38:16 -0500

    removing permissions on download ability

--- wisccc/views.py
+++ wisccc/views.py
@@ -160,7 +160,7 @@ def wisc_cc_home(request):
     return render(request, "wisccc/wisc_cc_home.html")
 
 
-@permission_required("wisccc.survery_manager", raise_exception=True)
+# @permission_required("wisccc.survery_manager", raise_exception=True)
 def wisccc_download_data(request):
     # qs = Survey.objects.raw(query)
     # return djqscsv.render_to_csv_response(qs)
