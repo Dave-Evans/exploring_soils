6a47861517af7d03a71a4afab06311b11b0246a6
Dave-Evans <evans.dave.michael@gmail.com>
2023-09-15 22:35:37 -0500

    was trimming off https for CSRF origins in sh script

--- infra/ansible/update_env_for_new_tier.sh
+++ infra/ansible/update_env_for_new_tier.sh
@@ -6,8 +6,8 @@ cd /home/ubuntu/exploring_soils
 ipaddress=$(curl ifconfig.me)
 if [ $1 == 'prod' ]; then
     sed -i "s/ALLOWED_HOSTS=.*/ALLOWED_HOSTS=$ipaddress,evansgeospatial.com,www.evansgeospatial.com/g" .env
-    sed -i "s/CSRF_TRUSTED_ORIGINS=.*/CSRF_TRUSTED_ORIGINS=evansgeospatial.com,www.evansgeospatial.com/g" .env
+    sed -i "s/CSRF_TRUSTED_ORIGINS=.*/CSRF_TRUSTED_ORIGINS=https://evansgeospatial.com,https://www.evansgeospatial.com/g" .env
 else
     sed -i "s/ALLOWED_HOSTS=.*/ALLOWED_HOSTS=$ipaddress,$1.evansgeospatial.com/g" .env
-    sed -i "s/CSRF_TRUSTED_ORIGINS=.*/CSRF_TRUSTED_ORIGINS=$1.evansgeospatial.com/g" .env
+    sed -i "s/CSRF_TRUSTED_ORIGINS=.*/CSRF_TRUSTED_ORIGINS=https://$1.evansgeospatial.com/g" .env
 fi
