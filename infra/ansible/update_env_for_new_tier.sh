65dc8c48f0887d3e9d1ec06003025cc8aa671b8b
Dave-Evans <evans.dave.michael@gmail.com>
2023-09-28 13:37:40 -0500

    more escapes for urls

--- infra/ansible/update_env_for_new_tier.sh
+++ infra/ansible/update_env_for_new_tier.sh
@@ -7,7 +7,7 @@ ipaddress=$(curl ifconfig.me)
 
 if [ $1 == 'prod' ]; then
     sed -i "s/ALLOWED_HOSTS=.*/ALLOWED_HOSTS=$ipaddress,evansgeospatial.com,www.evansgeospatial.com/g" .env
-    sed -i "s/CSRF_TRUSTED_ORIGINS=.*/CSRF_TRUSTED_ORIGINS=https://evansgeospatial.com,https://www.evansgeospatial.com/g" .env
+    sed -i "s/CSRF_TRUSTED_ORIGINS=.*/CSRF_TRUSTED_ORIGINS=https:\/\/evansgeospatial.com,https:\/\/www.evansgeospatial.com/g" .env
 else
     sed -i "s/ALLOWED_HOSTS=.*/ALLOWED_HOSTS=$ipaddress,$1.evansgeospatial.com/g" .env
     sed -i "s/CSRF_TRUSTED_ORIGINS=.*/CSRF_TRUSTED_ORIGINS=https:\/\/$1.evansgeospatial.com/g" .env
