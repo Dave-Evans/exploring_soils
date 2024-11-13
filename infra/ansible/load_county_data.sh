d36cffcd5e4493e5238ffd8ba7a1d281024014a5
Dave-Evans <evans.dave.michael@gmail.com>
2024-11-13 11:54:02 -0600

    added local lines, still commented out

--- infra/ansible/load_county_data.sh
+++ infra/ansible/load_county_data.sh
@@ -10,7 +10,8 @@ mkdir ./data/county_nrcs_a_wi.gdb
 aws s3 cp s3://$4/db/county_nrcs_a_mn.gdb/ ./data/county_nrcs_a_mn.gdb --recursive
 aws s3 cp s3://$4/db/county_nrcs_a_wi.gdb/ ./data/county_nrcs_a_wi.gdb --recursive
 # Load county data to database
-# ogr2ogr -overwrite -f PostgreSQL PG:"host=localhost dbname=db_davemike user=usr_davemike password=luggage12543" -t_srs EPSG:4326 -nln mn_counties ./data/county_nrcs_a_mn.gdb county_nrcs_a_mn
+# ogr2ogr -overwrite -f PostgreSQL PG:"host=localhost dbname=db_davemike user=usr_davemike password=pass" -t_srs EPSG:4326 -nln mn_counties ./data/county_nrcs_a_mn.gdb county_nrcs_a_mn
+# ogr2ogr -overwrite -f PostgreSQL PG:"host=localhost dbname=db_davemike user=usr_davemike password=pass" -t_srs EPSG:4326 -nln wi_counties ./data/county_nrcs_a_wi.gdb county_nrcs_a_wi
 ogr2ogr -overwrite -f PostgreSQL PG:"host=localhost dbname=$1 user=$2 password=$3" -t_srs EPSG:4326 -nln mn_counties ./data/county_nrcs_a_mn.gdb county_nrcs_a_mn
 ogr2ogr -overwrite -f PostgreSQL PG:"host=localhost dbname=$1 user=$2 password=$3" -t_srs EPSG:4326 -nln wi_counties ./data/county_nrcs_a_wi.gdb county_nrcs_a_wi
 
