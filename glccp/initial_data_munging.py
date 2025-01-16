6fdfe9acd04a4cc99e3f1f9371df057b3ef2d4ba
Dave-Evans <evans.dave.michael@gmail.com>
2025-01-16 11:22:27 -0600

    modified code for running on server

--- glccp/initial_data_munging.py
+++ glccp/initial_data_munging.py
@@ -7,10 +7,17 @@ from django.contrib.gis.geos import GEOSGeometry, Point
 
 from glccp.models import CleanedData
 
-fl_data = "~/Documents/glccp/Interactive Map_data.xlsx"
+fl_data = "~/Documents/glccp/Interactive Map_data_v2.xlsx"
 fl_zipcodes = "/home/evans/Documents/small_projects/wisc_cc/tl_2020_us_zcta520/tl_2020_us_zcta520.shp"
 
-zipcodes = geopandas.read_file(fl_zipcodes)
+fl_data = "/home/ubuntu/Interactive Map_data_v2.xlsx"
+fl_loc = "/home/ubuntu/farm_locations.csv"
+# fl_zipcodes = "/home/ubuntu/tl_2020_us_zcta520.shp"
+
+zipcodes = geopandas.read_file(
+    fl_zipcodes
+    # bbox=(-96.42389620756555, 37.18015493447337, -81.83489159751977, 45.94466927653207),
+)
 
 
 def create_models_doc():
@@ -64,31 +71,70 @@ def find_lat_long(zipcode, zipcodes):
         return None
     centroid = target_zip.centroid.reindex().iloc[0]
     sigma = 0.045
-    longitude = centroid.x + np.random.uniform(-sigma, -sigma, 1)[0]
-    latitude = centroid.y + np.random.uniform(-sigma, -sigma, 1)[0]
+    longitude = centroid.x + np.random.uniform(-sigma, sigma, 1)[0]
+    latitude = centroid.y + np.random.uniform(-sigma, sigma, 1)[0]
 
     pt = GEOSGeometry(f"POINT({longitude} {latitude})")
     pt.srid = 4269
-    return pt
+    return pt  # f"POINT({longitude} {latitude})"
 
 
 dat = pd.read_excel(fl_data)
 dat = dat.rename(columns={"%Cover": "percent_cover"})
 dat.columns = dat.columns.str.lower()
 no_location = []
+loc = pd.read_csv(fl_loc)
+dat = dat.merge(loc, on=["farm", "field", "year"])
 for i, row in dat.iterrows():
+
+    farm_id = row["farm"]
+    field_id = row["field"]
+    print(f"Farm field: {farm_id} {field_id}:")
     cleaned_data_record = CleanedData.objects.create()
 
     dict_row = row.to_dict()
     for field, value in dict_row.items():
+
         if pd.isna(value):
             value = None
+        # For local
+        # farm_location = find_lat_long(row.zipcode, zipcodes)
+        # if farm_location is None:
+        #     no_location.append(row)
+        # setattr(cleaned_data_record, field, value)
+        # cleaned_data_record.farm_location = GEOSGeometry(farm_location)
+        # cleaned_data_record.save()
 
-        setattr(cleaned_data_record, field, value)
+        # For remote
+        if field == "farm_location":
+            print(f"\tFarm location: {value}")
+            if value is not None:
+                pt = GEOSGeometry(value)
+                pt.srid = 4269
+                print(f"\tpt: {pt}")
+                setattr(cleaned_data_record, field, pt)
+        else:
+            setattr(cleaned_data_record, field, value)
+        cleaned_data_record.save()
 
-    farm_location = find_lat_long(row.zipcode, zipcodes)
-    if farm_location is None:
-        no_location.append(row)
-    cleaned_data_record.farm_location = farm_location
 
-    cleaned_data_record.save()
+## Investigating differences in data versions
+
+dat_v1 = pd.read_excel("~/Documents/glccp/Interactive Map_data.xlsx")
+dat_v2 = pd.read_excel("~/Documents/glccp/Interactive Map_data_v2.xlsx")
+
+col_diffs = dat_v1 == dat_v2
+
+for col in col_diffs:
+    if col_diffs[col].sum() != 253:
+        if col in ["CC_sampledate", "CC_plantdate"]:
+            continue
+        num_diffs = 253 - col_diffs[col].sum()
+        print(f"{num_diffs} differences in {col}")
+        print("\tVals in v1:")
+        print("\t", dat_v1[col].loc[~col_diffs[col]])
+        print("\tVals in v2:")
+        print("\t", dat_v2[col].loc[~col_diffs[col]])
+        rslt = input("Y to continue")
+        if rslt != "y":
+            break
