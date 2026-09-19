fb80baf09dbc64b16395f208495339b1d70d28e8
Dave-Evans <evans.dave.michael@gmail.com>
2026-09-19 14:29:44 -0500

    adding in marksafe for the pdf link

--- wisccc/forms_2025.py
+++ wisccc/forms_2025.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.utils.safestring import mark_safe
 from django.contrib.gis import forms as geo_forms
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
@@ -713,7 +714,9 @@ class SurveyFieldFormFull(forms.ModelForm):
     )
     # 45	Estimated cover crop planting cost per acre in this field. Please use UW Extension Custom Rate Guide.(https://www.nass.usda.gov/Statistics_by_State/Wisconsin/Publications/WI-CRate20.pdf)
     cover_crop_planting_cost = forms.IntegerField(
-        label='34. Estimated cover crop planting cost per acre in this field. Please use <a href="https://www.nass.usda.gov/Statistics_by_State/Wisconsin/Publications/WI-CRate20.pdf" target="_blank" rel="noopener noreferrer">UW Extension Custom Rate Guide.</a>',
+        label=mark_safe(
+            '34. Estimated cover crop planting cost per acre in this field. Please use <a href="https://www.nass.usda.gov/Statistics_by_State/Wisconsin/Publications/WI-CRate20.pdf" target="_blank" rel="noopener noreferrer">UW Extension Custom Rate Guide.</a>'
+        ),
         min_value=0,
         required=True,
     )
